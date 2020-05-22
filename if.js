8d06a9bad97e88071871bcb0ba8e591702f4fd0f
norvey <norveypv@gmail.com>
2020-05-22 06:40:39 -0500

    se agrega las llaves

--- if.js
+++ if.js
@@ -8,7 +8,7 @@ let y;
 
 //piedra le gana a la tijera
 //la tijera le gana al papel
-//el papel le gana a la piedra
+//el papel le gana a la piedraMath.floor(Math.random()*((3+1)-1+1))
 alert("Vamos a Jugar piedra papel o tijeras contra la consola");
 
 let escoge = prompt("Escoge 1 = piedra, 2 = papel, 3 = tijera");
@@ -20,28 +20,28 @@ resultados = document.body.innerHTML
 
 
 
-juego (x=escoge, y=Math.floor(Math.random()*((3+1)-1+1)));
+juego (x=escoge, y=1);
 
 
 
 function juego(x,y)
-
 {
-switch (escoge){
-case "1":
     if(x==piedra && y ==tijera)
     {
         document.body.innerHTML = "Tú ganas";
 
-    }else if(x==tijera && y== piedra)
+    }
+    else if(x==tijera && y== piedra)
     {
         document.body.innerHTML= " La Consola gana";
 
-    }else if(x==tijera && y ==papel)
+    }
+    else if(x==tijera && y ==papel)
     {
         document.body.innerHTML = "Tú ganas"
 
-    }else if(x==papel && y ==tijera)
+    }
+    else if(x==papel && y ==tijera)
     {
         document.body.innerHTML = "La consola gana";
     }
@@ -55,73 +55,10 @@ case "1":
         document.body.innerHTML = "Tú ganas";
 
     }
-    else{ document.body.innerHTML = "empatados";
-}
-break;
+    else if(x==y){document.body.innerHTML = "empatados";}
+    else if(x<1){document.body.innerHTML = "No es un valor correcto, debes ingresar un numero entre el 1 y 3, vuelve a intentarlo...";}
+    else if(x>3){document.body.innerHTML = "No es un valor correcto, debes ingresar un numero entre el 1 y 3, vuelve a intentarlo...";}
+    else if (x=true){document.body.innerHTML = "No es un valor correcto, debes ingresar un numero entre el 1 y 3, vuelve a intentarlo...";} 
+    else{ document.body.innerHTML = "Erro 404";}
+ }
 
-case "2":
-    if(x==piedra && y ==tijera)
-    {
-        document.body.innerHTML = "Tú ganas";
-
-    }else if(x==tijera && y== piedra)
-    {
-        document.body.innerHTML= " La Consola gana";
-
-    }else if(x==tijera && y ==papel)
-    {
-        document.body.innerHTML = "Tú ganas"
-
-    }else if(x==papel && y ==tijera)
-    {
-        document.body.innerHTML = "La consola gana";
-    }
-    else if(x== piedra && y ==papel)
-    {
-        document.body.innerHTML = "La consola gana";
-
-    }
-    else if(x==papel && y == piedra)
-    {
-        document.body.innerHTML = "Tú ganas";
-
-    }
-    else{ document.body.innerHTML = "empatados";
-}
-break;
-
-    
-case "3":
-    if(x==piedra && y ==tijera)
-    {
-        document.body.innerHTML = "Tú ganas";
-
-    }else if(x==tijera && y== piedra)
-    {
-        document.body.innerHTML= " La Consola gana";
-
-    }else if(x==tijera && y ==papel)
-    {
-        document.body.innerHTML = "Tú ganas"
-
-    }else if(x==papel && y ==tijera)
-    {
-        document.body.innerHTML = "La consola gana";
-    }
-    else if(x== piedra && y ==papel)
-    {
-        document.body.innerHTML = "La consola gana";
-
-    }
-    else if(x==papel && y == piedra)
-    {
-        document.body.innerHTML = "Tú ganas";
-
-    }
-    else{ document.body.innerHTML = "empatados";
-
-    }
-    break;
-    default:(document.body.innerHTML =" No has intriducido un valor correcto, vuelte a intentarlo."); break;
-}
-}
